9cdca616ddbf20f0359a08b21167acd185e9d17b
aishinchi <dengchengyun54@qq.com>
2018-03-28 17:44:55 +0800

    case 50%
    负号的优先级大于 =- ，因此造成minite一直错误。

--- ClockOfClass.py
+++ ClockOfClass.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
         minite = listOfTime[2*j+1] + needTime
         hour = listOfTime[2*j]
         if minite >= 60:
-            minite =- 60
+            minite = minite - 60
             hour =+ 1
         if hour > classTime[0] or (hour == classTime[0] and minite > classTime[1]):
             list2.append(10000)
