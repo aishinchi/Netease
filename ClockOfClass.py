9a2d6eade6f66cab5f22d2e2dbb34f717e15a636
aishinchi <dengchengyun54@qq.com>
2018-03-28 17:42:59 +0800

    60% case
    在 17 ： 17 这个case一直输出 0：0

---
+++ ClockOfClass.py
@@ -0,0 +1,31 @@
+import sys
+if __name__ == "__main__":
+    numberOfClock = int(sys.stdin.readline().strip())
+    listOfTime = []
+    for i in range(numberOfClock):
+        line = sys.stdin.readline().strip()
+        lines = line.split()
+        listOfTime.append(int(lines[0]))
+        listOfTime.append(int(lines[1]))
+    needTime = int(sys.stdin.readline().strip())
+    classTimetemp = sys.stdin.readline().strip().split()
+    classTime = []
+    classTime.append(int(classTimetemp[0]))
+    classTime.append(int(classTimetemp[1]))
+    list2 = []
+    for j in range(numberOfClock):
+        minite = listOfTime[2*j+1] + needTime
+        hour = listOfTime[2*j]
+        if minite >= 60:
+            minite =- 60
+            hour =+ 1
+        if hour > classTime[0] or (hour == classTime[0] and minite > classTime[1]):
+            list2.append(10000)
+        else:
+            if minite > classTime[1]:
+                lastTime = classTime[1] + 60 - minite + (classTime[0] - 1 - hour) * 60
+            else:
+                lastTime = classTime[1] - minite + (classTime[0] - hour) * 60
+            list2.append(lastTime)
+    number = list2.index(min(list2))
+    print(str(listOfTime[2*number]) + " " + str(listOfTime[2*number+1]))
